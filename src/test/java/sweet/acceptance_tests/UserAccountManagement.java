037a84fdc1af64c2bb2c14af43276ff1d4eaddcc
sabriNoor <nooraosama63@gmail.com>
2024-08-20 00:11:29 +0300

    fix bad smells

--- src/test/java/sweet/acceptance_tests/UserAccountManagement.java
+++ src/test/java/sweet/acceptance_tests/UserAccountManagement.java
@@ -7,10 +7,10 @@ import sweet.dev.models.User;
 
 import static org.junit.Assert.*;
 
-public class UserAcountManagement {
+public class UserAccountManagement {
     SweetApp obj = new SweetApp();
     String x;
-    String NewCityName;
+    String newCityName;
     User user1 = new User();
     @When("the user select edit city {string}")
     public void the_user_select_edit_city(String string) {
@@ -19,14 +19,14 @@ public class UserAcountManagement {
 
     @When("the user updates the {string} field with a new city name")
     public void the_user_updates_the_field_with_a_new_city_name(String string) {
-        NewCityName = string;
+        newCityName = string;
 
     }
 
     @Then("the system saves the new city")
     public void the_system_saves_the_new_city() {
         String OldCity=obj.getUsers().get(0).getAdress().getStreet();
-        obj.getUsers().get(0).getAdress().setCity(NewCityName);
+        obj.getUsers().get(0).getAdress().setCity(newCityName);
         assertNotEquals(OldCity,obj.getUsers().get(0).getAdress().getCity());
 
     }
@@ -35,19 +35,19 @@ public class UserAcountManagement {
     public void the_user_select_edit_street(String string) {
         x=string;
     }
-    String NewStreetName;
+    String newStreetName;
     @When("the user updates the {string} field with a new street name")
     public void the_user_updates_the_field_with_a_new_street_name(String string) {
-        NewStreetName = string;
+        newStreetName = string;
     }
 
     @Then("the system saves the new street")
     public void the_system_saves_the_new_street() {
-        String OldStreet=obj.getUsers().get(0).getAdress().getStreet();
-       obj.getUsers().get(0).getAdress().setStreet(NewStreetName);
-        assertNotEquals(OldStreet,obj.getUsers().get(0).getAdress().getStreet());
+        String oldStreet=obj.getUsers().get(0).getAdress().getStreet();
+       obj.getUsers().get(0).getAdress().setStreet(newStreetName);
+        assertNotEquals(oldStreet,obj.getUsers().get(0).getAdress().getStreet());
     }
-    String NewHomenumber;
+    String newHomeNumber;
     @When("the user select edit home number {string}")
     public void the_user_select_edit_home_number(String string) {
         x=string;
@@ -55,16 +55,16 @@ public class UserAcountManagement {
 
     @When("the user updates the {string} field with a new home number name")
     public void the_user_updates_the_field_with_a_new_home_number_name(String string) {
-        NewHomenumber = string;
+        newHomeNumber = string;
     }
 
     @Then("the system saves the new home number")
     public void the_system_saves_the_new_home_number() {
         String Oldhome=obj.getUsers().get(0).getAdress().getHomeNum();
-       obj.getUsers().get(0).getAdress().setHomeNum(NewHomenumber);
+       obj.getUsers().get(0).getAdress().setHomeNum(newHomeNumber);
         assertNotEquals(Oldhome,obj.getUsers().get(0).getAdress().getHomeNum());
     }
-    String NewPhoneNumber;
+    String newPhoneNumber;
     @When("the user select edit phone number {string}")
     public void the_user_select_edit_phone_number(String string) {
        x=string;
@@ -72,30 +72,30 @@ public class UserAcountManagement {
 
     @When("the user updates the {string} field with a new phone number name")
     public void the_user_updates_the_field_with_a_new_phone_number_name(String string) {
-        NewPhoneNumber = string;
+        newPhoneNumber = string;
     }
 
     @Then("the system saves the new phone number")
     public void the_system_saves_the_new_phone_number() {
-        String Oldphone=obj.getUsers().get(0).getPhoneNum();
-        assertTrue("Success",obj.getUsers().get(0).setPhoneNum(NewPhoneNumber));
-        assertNotEquals(Oldphone,obj.getUsers().get(0).getPhoneNum());
+        String oldPhone=obj.getUsers().get(0).getPhoneNum();
+        assertTrue("Success",obj.getUsers().get(0).setPhoneNum(newPhoneNumber));
+        assertNotEquals(oldPhone,obj.getUsers().get(0).getPhoneNum());
     }
 
     @When("the user select edit email {string}")
     public void the_user_select_edit_email(String string) {
         x=string;
     }
-    String NewEmail;
+    String newEmail;
     @When("the user updates the {string} field with a new email name")
     public void the_user_updates_the_field_with_a_new_email_name(String string) {
-        NewEmail = string;
+        newEmail = string;
     }
 
 
     @Then("the system saves the new email")
     public void theSystemSavesTheNewEmail() {
-        assertTrue("Success",obj.getUsers().get(0).setEmail(NewEmail));
+        assertTrue("Success",obj.getUsers().get(0).setEmail(newEmail));
 
     }
     @When("the user select edit password {string}")
