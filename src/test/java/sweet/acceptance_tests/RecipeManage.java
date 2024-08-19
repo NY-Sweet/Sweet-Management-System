d816767afeec33a02dfbbb9e5ee6e5ea515c29b1
sabriNoor <nooraosama63@gmail.com>
2024-08-20 00:06:33 +0300

    fix bad smells

--- src/test/java/sweet/acceptance_tests/RecipeManage.java
+++ src/test/java/sweet/acceptance_tests/RecipeManage.java
@@ -12,28 +12,28 @@ import static org.junit.Assert.*;
 public class RecipeManage {
 
     SweetApp obj = new SweetApp();
-    Recipe NewRecipe;
-    String Dtr;
-    String recipetosearch ;
-    String Content;
+    Recipe newRecipe;
+    String dtr;
+    String recipeToSearch;
+    String string;
 
     @When("the user {string} fills in the recipe details including name {string}, number of ingredients {int}, ingredients {string}, steps {string}")
     public void the_user_fills_in_the_recipe_details_including_name_number_of_ingredients_ingredients_steps(String string, String string2, int string3, String string4, String string5) {
-        NewRecipe = new Recipe(string2,string3,string4,string5,string);
-        obj.getRecipeManager().postRecipe(NewRecipe);
+        newRecipe = new Recipe(string2,string3,string4,string5,string);
+        obj.getRecipeManager().postRecipe(newRecipe);
 
     }
 
     @Then("the system validates the inputs")
     public void the_system_validates_the_inputs() {
-        Recipe NotValidated= obj.getRecipeManager().getnotValidatedRecipes().get(0);
-        assertTrue("Succeed", obj.getRecipeManager().validateRecipe(NotValidated));
+        Recipe notValidated= obj.getRecipeManager().getnotValidatedRecipes().get(0);
+        assertTrue("Succeed", obj.getRecipeManager().validateRecipe(notValidated));
 
     }
 
     @Then("if the details are valid, the recipe is created successfully")
     public void if_the_details_are_valid_the_recipe_is_created_successfully() {
-       assertTrue("Success", Objects.equals(obj.getRecipeManager().findRecipesByName(NewRecipe.getName()).get(0).getName(), NewRecipe.getName()));
+       assertEquals(obj.getRecipeManager().findRecipesByName(newRecipe.getName()).get(0).getName(), newRecipe.getName());
     }
 
     @Then("a  message is displayed")
@@ -41,12 +41,12 @@ public class RecipeManage {
         obj.messageAfterValedationOfTheRecipe();
         assertTrue("success",obj.isMessageDisplayedforVlidationaRecipe());
         obj.clearMessageDisplayedAfterRecipeValidation ();
-        obj.getRecipeManager().validateRecipe(NewRecipe);
+        obj.getRecipeManager().validateRecipe(newRecipe);
     }
 
     @When("the user select Browse Recipe")
     public void theUserSelectBrowseRecipe() {
-        //obj.chooseFromUserRecipePage(2);
+        //the user select Browse Recipe
     }
 
     @Then("Display all recipes from all users")
@@ -57,36 +57,36 @@ public class RecipeManage {
 
     @When("the user enter the {string}")
     public void the_user_enter_the(String string) {
-        Dtr= string ;
+        dtr = string ;
     }
 
     @Then("Display all recipes match")
     public void display_all_recipes_match() {
-      //  assertTrue("succeed",obj.recipeToSearch());
+      //  Display all recipes match
     }
 
     @Then("Display all recipes based on allergies")
     public void display_all_recipes_based_on_allergies() {
         Set<String> allergies = new HashSet<String>();
-        allergies.add(Dtr);
+        allergies.add(dtr);
         assertTrue("succeed", obj.getRecipeManager().filterRecipesByAllergies(obj.getRecipeManager().getValidatedRecipes(),allergies));
     }
 
     @Then("Display all recipes based on dietary")
     public void display_all_recipes_based_on_dietary() {
-        assertTrue("succeed", obj.getRecipeManager().filterRecipesByDietaryRestrictions(obj.getRecipeManager().getValidatedRecipes(), Dtr));
+        assertTrue("succeed", obj.getRecipeManager().filterRecipesByDietaryRestrictions(obj.getRecipeManager().getValidatedRecipes(), dtr));
 
     }
 
     @When("the user enter the {string} and not found any for him")
     public void the_user_enter_the_and_not_found_any_for_him(String string) {
-        Dtr= string;
+        dtr = string;
     }
 
 
     @Then("Print Nothing found suitable for you")
     public void print_nothing_found_suitable_for_you() {
-        assertFalse( obj.getRecipeManager().filterRecipesByDietaryRestrictions(obj.getRecipeManager().getValidatedRecipes(), Dtr));
+        assertFalse( obj.getRecipeManager().filterRecipesByDietaryRestrictions(obj.getRecipeManager().getValidatedRecipes(), dtr));
 
     }
 
@@ -94,17 +94,17 @@ public class RecipeManage {
 
     @When("the user enters the recipe Name {string} and feedback content {string}")
     public void theUserEntersTheRecipeNameAndFeedbackContent(String string, String string2) {
-        recipetosearch = string;
-        Content = string2;
+        recipeToSearch = string;
+        this.string = string2;
     }
 
     @Then("the feedback is sent successfully")
     public void the_feedback_is_sent_successfully() {
-        Recipe RE1 =  new Recipe("Pancake",3,"Milk, flour, oil","Mix them all","haya");
+        Recipe re1 =  new Recipe("Pancake",3,"Milk, flour, oil","Mix them all","haya");
 
-        obj.getRecipeManager().postRecipe(RE1);
-        obj.getRecipeManager().validateRecipe(RE1);
-        assertTrue("Succeed",obj.getRecipeManager().findRecipesByName("cake").get(0).addFeedback(Content) );
+        obj.getRecipeManager().postRecipe(re1);
+        obj.getRecipeManager().validateRecipe(re1);
+        assertTrue("Succeed",obj.getRecipeManager().findRecipesByName("cake").get(0).addFeedback(string) );
     }
 
 
